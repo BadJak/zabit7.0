e4ee6f3422a9bf965a9ebc80630b11823f3e326e
Basil JelalEldean <soli.geu@gmail.com>
2019-08-24 11:34:11 +0100

    banner ready

--- app/overrides/update_home.rb
+++ app/overrides/update_home.rb
@@ -3,7 +3,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
   :insert_after => 'div#spree-header',
   :text => '<div class="banner" >
   <div class="container">
-    <h1><strong>ZABYT</strong><strong>creative people</strong>!</h1>
+    <h1><strong>ZABYT</strong> <strong>creative people</strong>!</h1>
     <p>The revs Wear</p>
     <a class="btn btn-flat" href="#">Apply now</a>
   </div>
