e0125dc8ef8dc3874d914d8c6e7a0764d4b70ccb
Basil JelalEldean <soli.geu@gmail.com>
2019-08-24 17:45:02 +0100

    zabyt looks good

--- app/overrides/update_home.rb
+++ app/overrides/update_home.rb
@@ -3,9 +3,8 @@ Deface::Override.new(:virtual_path => 'spree/shared/_header',
   :insert_after => 'div#spree-header',
   :text => '<div class="banner" >
   <div class="container">
-    <h1><strong>ZABYT</strong> <strong>creative people</strong>!</h1>
-    <p>The revs Wear</p>
-    <a class="btn btn-flat" href="#">Apply now</a>
+    <h1><strong>Z A B Y T</strong> <strong></strong>!</h1>
+    <p>The Revolution Wear</p>
   </div>
 </div>
 ')
